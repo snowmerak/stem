e612843c687048c2443f43be20ac70568d1caab8
snowmerak <go_dev@kakao.com>
2021-02-10 10:55:57 +0900

    Update main.go

--- main.go
+++ main.go
@@ -5,18 +5,24 @@ import (
 	"os"
 )
 
-var cmdMap map[string]func(...string) error
-
-func init() {
+func main() {
+	var cmdMap map[string]func(...string) error
 	cmdMap = map[string]func(...string) error{}
 	cmdMap["launch"] = launch
 	cmdMap["init"] = makeSeed
-}
+	cmdMap["list"] = list
+	cmdMap["remove"] = remove
+	cmdMap["install"] = install
+	cmdMap["insert"] = insert
 
-func main() {
 	if len(os.Args) < 3 {
 		fmt.Println("too few")
 		return
 	}
-	fmt.Println(cmdMap[os.Args[1]](os.Args[2:]...))
+	v, ok := cmdMap[os.Args[1]]
+	if !ok {
+		fmt.Println("command not found")
+		return
+	}
+	fmt.Println(v(os.Args[2:]...))
 }
